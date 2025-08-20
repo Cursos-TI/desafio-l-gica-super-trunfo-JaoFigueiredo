85e3eec55ccd607bc852bba2a0b557a30541e5e5
JaoFigueiredo <134517389+JaoFigueiredo@users.noreply.github.com>
2025-08-20 13:04:40 +0000

    Realizei todo o desafio basico

--- logicaSuperTrunfo.c
+++ logicaSuperTrunfo.c
@@ -14,6 +14,9 @@ int main(){                                  //todas inicializadas para evitar o
     float superpoder1=0.0,superpoder2=0.0;  //Declaração das variáveis para o campo Super poder
     float inverso1=0.0,inverso2=0.0;        //Declaração das variáveis de suporte para gravar o inverso da densidade
 
+    
+    // Conjunto de printf e scanf para receber os dados da  carta 1 #foco na linha 26 onde nao utiliza-se o & por ser uma string
+
     printf("Bem vindo ao cadastro de cartas do Desafio Super Trunfo!\n\n Por favor digite uma letra entre A e H \n");
     scanf("%c",&estado1);                                                                                           
 
@@ -35,28 +38,36 @@ int main(){                                  //todas inicializadas para evitar o
     printf("Por fim, digite o número de pontos túristicos desta cidade\n");                                         
     scanf("%i",&pontos1);
 
-    getchar(); //Comando para "consumir o enter" Pois o próximo scanf sera com valor %c
+    //Comando para "consumir o enter" Pois o próximo scanf sera com valor %c
 
-    printf("Agora cadastraremos a segunda carta!\n\n Por favor digite uma letra entre A e H \n");                   //Mensagem p/o usuário
-    scanf("%c",&estado2);//comando para entrada de dados
+    getchar(); 
 
-    printf("Agora digite um número, 01, 02, 03 ou 04 (Com o Zero)\n");                                              //Mensagem p/o usuário            
-    scanf("%d",&codigo2);//comando para entrada de dados
+    // Conjunto de printf e scanf para receber os dados da carta 2 foco na linha 54 onde nao utiliza-se o & por ser uma string
+    
+    printf("Agora cadastraremos a segunda carta!\n\n Por favor digite uma letra entre A e H \n");                   
+    scanf("%c",&estado2);
 
-    printf("Agora digite o nome da cidade\n");                                                                       //Mensagem p/o usuário
-    scanf(" %[^\n]",cidade2);//comando para entrada de dados
+    printf("Agora digite um número, 01, 02, 03 ou 04 (Com o Zero)\n");                                                         
+    scanf("%d",&codigo2);
 
-    printf("Agora digite a população desta cidade\n");                                                              //Mensagem p/o usuário
-    scanf("%lu",&populacao2);//comando para entrada de dados
+    printf("Agora digite o nome da cidade\n");                                                                       
+    scanf(" %[^\n]",cidade2);
 
-    printf("Agora digite o PIB desta cidade\n");                                                                    //Mensagem p/o usuário
-    scanf("%f",&pib2);//comando para entrada de dados
+    printf("Agora digite a população desta cidade\n");                                                              
+    scanf("%lu",&populacao2);
 
-    printf("Agora digite a área desta cidade\n");                                                                    //Mensagem p/o usuário
-    scanf("%f",&area2);//comando para entrada de dados
+    printf("Agora digite o PIB desta cidade\n");                                                                    
+    scanf("%f",&pib2);
 
-    printf("Por fim, digite o número de pontos túristicos desta cidade\n");                                         //Mensagem p/o usuário
-    scanf("%i",&pontos2);//comando para entrada de dados
+    printf("Agora digite a área desta cidade\n");                                                                    
+    scanf("%f",&area2);
+
+    printf("Por fim, digite o número de pontos túristicos desta cidade\n");                                         
+    scanf("%i",&pontos2);
+
+    /*Utilização de operadores para calculo dos outros paramentros das cartas, achei mais facil utilizar
+    uma variavel para recebero inverso da densidade do que utilizar no codigo 1/densidade*/
+    
 
     pibpercapt1 = (float) pib1 / populacao1;
     pibpercapt2 = (float) pib2 / populacao2;
@@ -70,7 +81,9 @@ int main(){                                  //todas inicializadas para evitar o
     superpoder1= (unsigned long) populacao1 + pontos1 + area1 + pib1 + pibpercapt1 + inverso1;
     superpoder2= (unsigned long) populacao2 + pontos2 + area2 + pib2 + pibpercapt2 + inverso2;
     
-    printf("Carta 1 \n");       //Sequencia de prints para imprimir o resultado final da carta 1
+    //Sequencia de prints para imprimir o resultado final da carta 1
+    
+    printf("Carta 1 \n");       
     printf("%s",cidade1);
     printf("  ");
     printf("%c",estado1);
@@ -82,7 +95,9 @@ int main(){                                  //todas inicializadas para evitar o
     printf("Densidade Populacional: %.2f Hab/Km²\n",densidade1);
     printf("PIB per capta: %.2f Reais\n\n",pibpercapt1);
 
-    printf("Carta 2 \n");       //Sequencia de prints para imprimir o resultado final da carta 2
+    //Sequencia de prints para imprimir o resultado final da carta 2
+
+    printf("Carta 2 \n");       
     printf("%s",cidade2);
     printf("  ");
     printf("%c",estado2);
@@ -94,5 +109,56 @@ int main(){                                  //todas inicializadas para evitar o
     printf("Densidade Populacional: %.2f Hab/Km²\n",densidade2);
     printf("PIB per capta: %.2f Reais\n\n",pibpercapt2);
   
+    /*Na video aula, o professor disse que eu poderia realizar a comparação de todos os parametros
+    Então utilizei a estrutura if/else para indicar todos os parametros vencedores*/ 
+
+    printf("Comparando populações\n");
+    
+    if (populacao1>populacao2)
+        printf("A Carta 1 é a vencedora\n\n");
+    else
+        printf("A Carta 2 é a vencedora\n\n");
+
+    printf("Comparando Pontos turísticos\n");
+    
+    if (pontos1>pontos2)
+        printf("A Carta 1 é a vencedora\n\n");
+    else
+        printf("A Carta 2 é a vencedora\n\n");
+
+    printf("Comparando Área da cidade\n");
+    
+    if (area1>area2)
+        printf("A Carta 1 é a vencedora\n\n");
+    else
+        printf("A Carta 2 é a vencedora\n\n");
+    
+    printf("Comparando PIB\n");
+    
+    if (pib1>pib2)
+        printf("A Carta 1 é a vencedora\n\n");
+    else
+        printf("A Carta 2 é a vencedora\n\n");
+        
+    printf("Comparando PIB per Capta\n");
+    
+    if (pibpercapt1>pibpercapt2)
+        printf("A Carta 1 é a vencedora\n\n");
+    else
+        printf("A Carta 2 é a vencedora\n\n");
+        
+    printf("Comparando Densidade\n");
+    
+    if (densidade1<densidade2)
+        printf("A Carta 1 é a vencedora\n\n");
+    else
+        printf("A Carta 2 é a vencedora\n\n");
+        
+    printf("Comparando Super Poder\n");
+    
+    if (superpoder1>superpoder2)
+        printf("A Carta 1 é a vencedora\n\n");
+    else
+        printf("A Carta 2 é a vencedora\n\n");    
 return 0;
 }
